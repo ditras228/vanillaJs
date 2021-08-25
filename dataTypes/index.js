b87fa9984d03e5a8c2fe7b1d8d110e62a4c8baf4
Dmitry Druzinin <78543687+ditras228@users.noreply.github.com>
2021-08-25 07:51:23 +0300

    Initial commit

---
+++ dataTypes/index.js
@@ -0,0 +1,9 @@
+export const dataTypes = {
+    string:'string',
+    symbol:'symbol',
+    number:'number',
+    bigint:'bigint',
+    boolean:'boolean',
+    null: 'null',
+    undefined: 'undefined'
+}
